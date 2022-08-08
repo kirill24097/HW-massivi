20f817e3aa780954c5580e4a210aed40b973856a
kirill24097 <110169552+kirill24097@users.noreply.github.com>
2022-08-08 10:39:40 +0300

    massiviHoumWork

--- src/Main.java
+++ src/Main.java
@@ -1,32 +1,51 @@
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args){
         int[] arr = generateRandomArray();
 
+///1
         int sum = 0;
         for (int daySuumma : arr) {
             sum = sum + daySuumma;
         }
-            System.out.println("Сумма трат за месяц составила   " + sum + "   рублей");
+        System.out.println("Сумма трат за месяц составила   " + sum + "   рублей");
+        ///2
 
-    }
-        public static int[] generateRandomArray() {
-                java.util.Random random = new java.util.Random();
-                int[] arr = new int[30];
-                for (int i = 0; i < arr.length; i++) {
-                    arr[i] = random.nextInt(100_000) + 100_000;
-                }
-                return arr;
-
-
+        int minSuma = arr[0];
+        int maksSuma = arr[0];
+        for (int i = 1; i < arr.length; i++) {
+            if (minSuma > arr[i]) {
+                minSuma = arr[i];
+            } else if (maksSuma < arr[i]) {
+                maksSuma = arr[i];
+            }
 
         }
 
+        System.out.println("Минимальная сумма трат за день составила  " + minSuma + "  рублей.  " +
+                "Максимальная сумма трат за день составила  " + maksSuma + "  рублей");
 
+        ///3
+        double srednyaSummaZaVtsyats = (double) sum / arr.length;
+        {
+            System.out.println("Средняя сумма трат за месяц составила  " + srednyaSummaZaVtsyats + "  рублей");
+        }
+        ///4
 
+        char[] reverseFullName = {'n', 'a', 'v', 'I', ' ', 'v', 'o', 'n', 'a', 'v', 'I'};
+        for (int i = reverseFullName.length - 1; i >= 0; i--) {
+            System.out.print(reverseFullName[i]);
+        }
 
+    }
 
-
-
+    public static int[] generateRandomArray() {
+        java.util.Random random = new java.util.Random();
+        int[] arr = new int[30];
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = random.nextInt(100_000) + 100_000;
+        }
+        return arr;
+    }
 
 
 
